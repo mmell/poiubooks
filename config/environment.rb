04b7a12ff6f157451644eb28909763c4811ed7f4
Mike Mell <mike@linksafe.name>
2010-09-24 17:30:12 -0700

    remove hpricot from config

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,6 @@ Rails::Initializer.run do |config|
   config.gem "restful_authentication"
   config.gem "acts_as_state_machine"
   config.gem 'aasm'
-  config.gem 'hpricot'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
  # :all can be used as a placeholder for all plugins not explicitly named
